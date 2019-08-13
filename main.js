15b56b31e392b41b4c987f14b92476063508a72d
Matthew Malone <m.malone@me.com>
2019-08-13 17:50:27 -0600

    Create counter to keep track of flicks
    Co-authored-by: Quinne Farenwald <q.farenwald@gmail.com>

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-  var fist = "images/fistToOne_logo-0.svg"
-  var one = "images/fistToOne_logo-1.svg"
-  var colors = ["#5F5EBC", "#8FD9D9", "#F2C948", "#D93B18", "#03A678"];
+var fist = "images/fistToOne_logo-0.svg"
+var one = "images/fistToOne_logo-1.svg"
+var colors = ["#5F5EBC", "#8FD9D9", "#F2C948", "#D93B18", "#03A678"];
+var flickCounter = 0;
 
-  $('.main__img-fist').on('click', () => {
-    changeFist();
-  })
+$('.main__img-fist').on('click', () => {
+  changeFist();
+})
 
-  const changeFist = () => {
-    if ($('.main__img-fist').attr('src') == fist) {
-      changeColor();
-      $('.main__img-fist').attr('src', one)
-    } else {
-      changeColor();
-      $('.main__img-fist').attr('src', fist)
-    }
+const changeFist = () => {
+  if ($('.main__img-fist').attr('src') == fist) {
+    changeColor();
+    countFlicks();
+    $('.main__img-fist').attr('src', one)
+  } else {
+    changeColor();
+    $('.main__img-fist').attr('src', fist)
   }
+}
 
-  const changeColor = () => {
-    if ($('.main__img-fist').attr('src') == fist) {
-      console.log('should be color')
-      $('body').css('background-color', getRandomColor())
-    } else {
-      console.log('should be white')
-      $('body').css('background-color', "#FFFFFF")
-    }
+const changeColor = () => {
+  if ($('.main__img-fist').attr('src') == fist) {
+    $('body').css('background-color', getRandomColor())
+  } else {
+    $('body').css('background-color', "#FFFFFF")
   }
+}
 
-  const getRandomColor = () => {
-    let index = Math.floor(Math.random() * colors.length);
-    return colors[index];
-  }
+const getRandomColor = () => {
+  let index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
+
+const countFlicks = () =>  {
+  flickCounter++;
+}
